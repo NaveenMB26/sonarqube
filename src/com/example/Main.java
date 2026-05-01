9825fccd3a427f3cb4b91b4712835d5d06bd26b1
NaveenMB26 <121723246+NaveenMB26@users.noreply.github.com>
2026-05-02 00:00:10 +0530

    Use logger instead of System.out.println
    Replaced System.out.println with logger for better logging practices.

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,13 +1,13 @@
+package com.example;
+
 import java.util.logging.Logger;
 
 public class Main {
 
-    // Create Logger instance
     private static final Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
 
-        // Replace System.out.println with logger
         logger.info("Application started");
 
         int a = 10;
