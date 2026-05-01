2c1dbc206027736ef6480a9eda119a4b6d24a75a
NaveenMB26 <121723246+NaveenMB26@users.noreply.github.com>
2026-05-01 23:25:26 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,21 @@
+package com.example;
+
+import java.util.logging.Logger;
+
 public class Main {
+
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
+
         int a = 10;
         int b = 0;
 
-        // Bug: Division by zero
-        System.out.println("Result: " + (a / b));
-
-        // Code smell: Unused variable
-        int unused = 100;
-
-        // Duplicate code
-        if (a > 5) {
-            System.out.println("A is greater than 5 number");
-        }
-        if (a > 5) {
-            System.out.println("A is greater than 5 number");
+        if (b != 0) {
+            int result = a / b;
+            logger.info("Result: " + result);
+        } else {
+            logger.warning("Cannot divide by zero");
         }
     }
 }
